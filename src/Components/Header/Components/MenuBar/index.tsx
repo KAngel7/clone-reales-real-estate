b7409f58778759d7ad67229ac95ed8d548f620f5
Khanh <huykhanh.cse@gmail.com>
2017-10-06 16:32:31 +0700

    DEV|Khanh|Update Header

--- src/Components/Header/Components/MenuBar/index.tsx
+++ src/Components/Header/Components/MenuBar/index.tsx
@@ -24,21 +24,21 @@ class MenuBar extends React.Component<{}, MenuBarState> {
         <a href="/">
           <div className="homeLogo osLight">
             <Icon name="home" size="2x" /> 
-            <span>Reales</span>
+            <span>House</span>
           </div>
         </a>
         <a 
           href="#" 
-          className={'home-navHandler visible-xs' + (this.state.isHandlerActive ? ' active' : '')} 
+          className={'homeNavHandler visible-xs' + (this.state.isHandlerActive ? ' active' : '')} 
           onClick={this.toggleHandler}
         >
           <Icon name="bars" />
         </a>
-        <div className={'home-nav' + (this.state.isHandlerActive ? ' active' : '')} >
+        <div className={'homeNav' + (this.state.isHandlerActive ? ' active' : '')} >
           <ul>
-            <li><a href="#">Buy</a></li>
-            <li><a href="#">Rent</a></li>
-            <li><a href="#">Find agent</a></li>
+            <li className="moreOption"><a href="#">Buy</a></li>
+            <li className="moreOption"><a href="#">Rent</a></li>
+            <li className="moreOption"><a href="#">Find agent</a></li>
             <li><a href="#">Sign Up</a></li>
             <li><a href="#">Sign In</a></li>
             <li><a className="btn btn-green">List a Property</a></li>
