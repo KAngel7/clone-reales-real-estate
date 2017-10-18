e9ce80bf45e8845568ca56e82c0c0bb721e6493f
tmkhangit95 <tmkhangit95@gmail.com>
2017-10-18 14:11:11 +0700

    DEV|KHANG|NEWPROPERTY

---
+++ src/Scenes/NewPropertyPage/Components/AddNewPropertyForm/index.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import './style.css';
+import { Icon } from 'react-fa';
+import SelectComponent from '../../../../Components/SelectComponent';
+import CheckBox from '../../../../Components/CheckBox';
+
+class AddNewPropertyForm extends React.Component<{}, {}> {
+  types = [
+    'For Sale',
+    'For Rent'
+  ];
+  render() {
+    return (
+      <div className="newPropertyForm">
+        <div className="row form-group">
+          <div className="title col-xs-12 col-sm-6 col-md-6">
+            <h4>Title</h4>
+            <input type="text" name="" id="" className="form-control" />
+          </div>
+          <div className="Price col-xs-12 col-sm-6 col-md-6">
+            <h4>Price</h4>
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <input type="text" name="" id="" className="form-control" />
+            </div>
+          </div>
+        </div>
+        <div className="row form-group">
+          <div className="discription col-xs-12 col-sm-12 col-md-12">
+            <h4>Description</h4>
+            <textarea className="form-control"></textarea>
+          </div>
+        </div>
+        <div className="row form-group">
+          <div className="address col-xs-12 col-sm-12 col-md-12">
+            <h4>Address</h4>
+            <input type="text" name="" id="" className="form-control" placeholder="Enter a Location" />
+          </div>
+        </div>
+        <div className="row form-group">
+          <div className="bedrooms col-xs-12 col-sm-6 col-md-3">
+            <h4>Bedrooms</h4>
+            <input type="text" name="" id="" className="form-control" />
+          </div>
+          <div className="bathrooms col-xs-12 col-sm-6 col-md-3">
+            <h4>Bathrooms</h4>
+            <input type="text" name="" id="" className="form-control" />
+          </div>
+          <div className="area col-xs-12 col-sm-6 col-md-3">
+            <h4>Area</h4>
+            <div className="input-group">
+              <input type="text" name="" id="" className="form-control" />
+              <span className="input-group-addon">Sq Ft</span>
+            </div>
+          </div>
+          <div className="type col-xs-12 col-sm-6 col-md-3">
+            <h4>Type</h4>
+            <SelectComponent listItem={this.types} />
+          </div>
+        </div>
+        <div className="row form-group">
+          <div className="imageGallery col-xs-12 col-sm-12 col-md-12">
+            <h4>Image Gallery</h4>
+            <div className="file-input file-input-new">
+              <input type="hidden" value="" />
+              <div className="file-preview">
+                <div className="file-preview-status text-center text-success" />
+                <div className="close fileinput-remove text-right">×</div>
+                <div className="file-preview-thumbnails" />
+                <div className="clearfix" />
+              </div>
+              <div className="btn btn-o btn-default btn-file">
+                <Icon name="folder-open" />
+                &nbsp;Browse Images
+                <input type="file" className="file" multiple={true} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row form-group">
+          <div className="amenities col-xs-12 col-sm-12 col-md-12">
+            <h4>Amenities</h4>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Garage</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Outdoor Pool</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Garden</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Security System</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Internet</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Telephone</CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Air Conditioning  </CheckBox>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-3">
+              <CheckBox>Heating</CheckBox>
+            </div>
+          </div>
+        </div>
+        <div className="row form-group rowBtn">
+          <input type="text" name="" id="" className="btn btn-green btn-lg" value="Add Property"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddNewPropertyForm;
--- src/Scenes/NewPropertyPage/index.tsx
+++ src/Scenes/NewPropertyPage/index.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import './style.css';
-import { RouteComponentProps } from 'react-router-dom';
 import Dashboard from '../../Components/DashboardLayout';
+import AddNewPropertyForm from './Components/AddNewPropertyForm';
+import { RouteComponentProps } from 'react-router-dom';
 
 class NewPropertyPage extends React.Component<RouteComponentProps<any>, {}> {
   render() {
     return (
-      <div className="newPropertyPage">
-        <Dashboard>
-          <div className="dashboardTitle">
-            <h3>New Property</h3>
+      <div className="infoPage">
+        <Dashboard >
+          <div className="infoWrapper">
+            <div className="dashboardTitle">
+                <h3>Add new property</h3>
+                <h5>We'd love to find out more about you. It'll help us make
+                sure our website and apps tick the right boxes.</h5>
+            </div>
+            <AddNewPropertyForm />
           </div>
         </Dashboard>
       </div>
